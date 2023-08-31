c89b2c423de3f4fae496df38dccd34ad921bb934
Kms6666 <kms66666@icloud.com>
2023-08-31 13:17:04 -0700

    Update ExamplePython.py
    Added jobs list

--- ExamplePython.py
+++ ExamplePython.py
@@ -1,4 +1,18 @@
+a = "string 1"
+b = "string 2"
+print("a = " + a + " and b = " + b)
+#how we learned
+
 x = "string 3"
-y = "String 4"
+y = "string 4"
 print(f"x = {x} and y = {y}")
+#Using F-string
+
+#also possible
+print("c = {c} and d = {d}".format(c=1, d=2))
+
+job_list = {'John': 'Doctor', 'Jane': 'Engineer', 'Jim': 'Teacher'}
+
+for name, job in job_list.items():
+    print(f"{name} is a {job}")
 
